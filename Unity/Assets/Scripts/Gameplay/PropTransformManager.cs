132cc47f9899e98a97beb69934980e5396b7338e
Tate McCormick <tatelax@gmail.com>
2021-10-30 00:35:19 -0400

    Add scale transform

--- Unity/Assets/Scripts/Gameplay/PropTransformManager.cs
+++ Unity/Assets/Scripts/Gameplay/PropTransformManager.cs
@@ -8,48 +8,55 @@ namespace Gameplay
     {
         [Header("References")]
         [SerializeField] private PropSpawnManager propSpawnManager;
+        [SerializeField] private float minScale, maxScale;
+
+        private RotateGestureRecognizer rotationGesture;
+        private ScaleGestureRecognizer scaleGesture;
         
-        private float rotationDegressDelta;
-
-        /// <summary>
-        /// Rotation gesture
-        /// </summary>
-        private RotateGestureRecognizer RotationGesture { get; set; }
-
         private void OnEnable()
         {
-            RotationGesture = new RotateGestureRecognizer();
-            RotationGesture.StateUpdated += RotationGesture_Updated;
-            FingersScript.Instance.AddGesture(RotationGesture);
+            rotationGesture = new RotateGestureRecognizer();
+            scaleGesture = new ScaleGestureRecognizer();
+            
+            rotationGesture.StateUpdated += RotationGesture_Updated;
+            scaleGesture.StateUpdated += ScaleGesture_Updated;
+
+            FingersScript.Instance.AddGesture(rotationGesture);
+            FingersScript.Instance.AddGesture(scaleGesture);
         }
-        
+
         private void OnDisable()
         {
             if (FingersScript.HasInstance)
             {
-                FingersScript.Instance.RemoveGesture(RotationGesture);
+                FingersScript.Instance.RemoveGesture(rotationGesture);
+                FingersScript.Instance.RemoveGesture(scaleGesture);
             }
         }
         
         private void RotationGesture_Updated(DigitalRubyShared.GestureRecognizer gesture)
         {
+            if (!propSpawnManager.CurrentlyPlacing) return;
+            
             if (gesture.State == GestureRecognizerState.Executing)
             {
-                rotationDegressDelta = RotationGesture.RotationDegreesDelta;
-            }
-            else
-            {
-                rotationDegressDelta = 0;
+                Vector3 newRot = propSpawnManager.CurrentlyPlacing.transform.rotation.eulerAngles;
+                newRot.y -= rotationGesture.RotationDegreesDelta;
+                propSpawnManager.CurrentlyPlacing.transform.rotation = Quaternion.Euler(newRot);
             }
         }
-
-        private void Update()
+        
+        private void ScaleGesture_Updated(GestureRecognizer gesture)
         {
             if (!propSpawnManager.CurrentlyPlacing) return;
             
-            Vector3 newRot = propSpawnManager.CurrentlyPlacing.transform.rotation.eulerAngles;
-            newRot.y -= rotationDegressDelta;
-            propSpawnManager.CurrentlyPlacing.transform.rotation = Quaternion.Euler(newRot);
+            if (gesture.State == GestureRecognizerState.Executing)
+            {
+                Vector3 currScale = propSpawnManager.CurrentlyPlacing.transform.localScale;
+                
+                if(currScale.x > minScale && currScale.x < maxScale)
+                    propSpawnManager.CurrentlyPlacing.transform.localScale *= scaleGesture.ScaleMultiplier;
+            }
         }
     }
 }
